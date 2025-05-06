352c0ab943c3069e2848c5e5d7c55962ffe97cc5
Jaydohe <101892786+jaydohe@users.noreply.github.com>
2025-05-07 02:55:51 +0700

    #37 change to cloud

--- src/API/SI.Infrastructure/Persistence/SIDbContext.cs
+++ src/API/SI.Infrastructure/Persistence/SIDbContext.cs
@@ -100,7 +100,7 @@ public class SIDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // dev
-        var connectionForMigration = "Server=localhost;Database=si.dev;User=root;Password=Devbyjelly123;";
+        var connectionForMigration = "Server=34.87.166.154;Database=sys_dev;User=root;Password=Devbyjelly123;";
 
         if (!optionsBuilder.IsConfigured)
         {
