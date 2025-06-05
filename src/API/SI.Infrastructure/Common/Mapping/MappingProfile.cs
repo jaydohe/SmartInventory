04608f5d83a39dbb07a00850be92dffb89c0d6c2
Jaydohe <101892786+jaydohe@users.noreply.github.com>
2025-06-06 01:07:07 +0700

    hot fix get all demand

--- src/API/SI.Infrastructure/Common/Mapping/MappingProfile.cs
+++ src/API/SI.Infrastructure/Common/Mapping/MappingProfile.cs
@@ -10,5 +10,73 @@ public class MappingProfile : Profile
     {
         CreateMap<User, UserDTO>()
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
+
+        CreateMap<Inventory, InventoryDTO>()
+            .ForMember(dest => dest.ProductId,
+                       opt => opt.MapFrom(src => src.ProductId))
+            .ForMember(dest => dest.ProductName,
+                       opt => opt.MapFrom(src => src.Product!.Name))
+            .ForMember(dest => dest.ProductUnit,
+                       opt => opt.MapFrom(src => src.Product!.Unit))
+            .ForMember(dest => dest.WarehouseId,
+                       opt => opt.MapFrom(src => src.WarehouseId))
+            .ForMember(dest => dest.WarehouseName,
+                       opt => opt.MapFrom(src => src.Warehouse!.Name))
+            .ForMember(dest => dest.Quantity,
+                       opt => opt.MapFrom(src => src.Quantity))
+            .ForMember(dest => dest.CreatedAt,
+                       opt => opt.MapFrom(src => src.CreatedAt));
+
+        CreateMap<Forecast, AllDemandForecastResponse>()
+            .ForMember(dest => dest.ProductId,
+                        opt => opt.MapFrom(src => src.ProductId))
+            .ForMember(dest => dest.ProductName,
+                       opt => opt.MapFrom(src => src.Product!.Name))
+            .ForMember(dest => dest.ProductUnit,
+                       opt => opt.MapFrom(src => src.Product!.Unit))
+            .ForMember(dest => dest.WarehouseId,
+                       opt => opt.MapFrom(src => src.WarehouseId))
+            .ForMember(dest => dest.WarehouseName,
+                       opt => opt.MapFrom(src => src.Warehouse!.Name))
+            .ForMember(dest => dest.Period,
+                       opt => opt.MapFrom(src => src.Period))
+            .ForMember(dest => dest.ForecastValue,
+                       opt => opt.MapFrom(src => src.ForecastValue))
+            .ForMember(dest => dest.Method,
+                        opt => opt.MapFrom(src => src.Method))
+            .ForMember(dest => dest.Trend,
+                       opt => opt.MapFrom(src => src.Trend))
+            .ForMember(dest => dest.Seasonal,
+                       opt => opt.MapFrom(src => src.Seasonal))
+            .ForMember(dest => dest.SeasonalityPeriod,
+                       opt => opt.MapFrom(src => src.SeasonalityPeriod))
+            .ForMember(dest => dest.LowerBound,
+                       opt => opt.MapFrom(src => src.LowerBound))
+            .ForMember(dest => dest.UpperBound,
+                       opt => opt.MapFrom(src => src.UpperBound))
+            .ForMember(dest => dest.CreatedAt,
+                       opt => opt.MapFrom(src => src.CreatedAt));
+
+        CreateMap<Forecast, InventoryOptimizationResponse>()
+            .ForMember(dest => dest.ProductId,
+                       opt => opt.MapFrom(src => src.ProductId))
+            .ForMember(dest => dest.ProductName,
+                       opt => opt.MapFrom(src => src.Product!.Name))
+            .ForMember(dest => dest.ProductUnit,
+                       opt => opt.MapFrom(src => src.Product!.Unit))
+            .ForMember(dest => dest.WarehouseId,
+                       opt => opt.MapFrom(src => src.WarehouseId))
+            .ForMember(dest => dest.WarehouseName,
+                       opt => opt.MapFrom(src => src.Warehouse!.Name))
+            .ForMember(dest => dest.Method,
+                       opt => opt.MapFrom(src => src.Method))
+            .ForMember(dest => dest.EOQ,
+                       opt => opt.MapFrom(src => src.EOQ))
+            .ForMember(dest => dest.SafetyStock,
+                        opt => opt.MapFrom(src => src.SafetyStock))
+            .ForMember(dest => dest.OptimalInventory,
+                        opt => opt.MapFrom(src => src.OptimalInventory))
+            .ForMember(dest => dest.CreatedAt,
+                        opt => opt.MapFrom(src => src.CreatedAt));
     }
 }
